695f6d3b46785254e4c8fbf1dbf85c2e0ec23b9f
Thomas Ahlström <thomas.ahlstrom@student.forsbergsskola.se>
2020-10-14 15:23:34 +0200

    User followed by camera and background visible

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+    public Transform followTransform;
+
+    void FixedUpdate() {
+        this.transform.position = new Vector3(
+            followTransform.position.x,followTransform.position.y, this.transform.position.z);
+    }
+}
